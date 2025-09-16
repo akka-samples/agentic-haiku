9e7e793b6f0128cbaddfe9c90e3a2b60cb9b02b0
Renato Cavalcanti <renato@cavalcanti.io>
2025-09-16 09:22:24 +0200

    feat: stream workflow progress as SSE (#8)
    * feat: stream workflow progress as SSE
    
    * timeout step and image
    
    * Page design based on Akka theme
    
    * form submition with progress

--- src/main/java/akka/haiku/generator/domain/ContentGeneration.java
+++ src/main/java/akka/haiku/generator/domain/ContentGeneration.java
@@ -1,27 +1,44 @@
 package akka.haiku.generator.domain;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-public record ContentGeneration(String userInput, Long generatedAt, Optional<Haiku> haiku, Optional<Image> image) {
+public record ContentGeneration(String userInput,
+                                Long generatedAt,
+                                List<String> progress,
+                                Optional<Haiku> haiku,
+                                Optional<Image> image) {
 
   public static ContentGeneration empty() {
-    return new ContentGeneration("***********", null, Optional.empty(), Optional.empty());
-  }
-
-  public ContentGeneration withImagePrompt(String userInput) {
-    return new ContentGeneration(userInput, generatedAt, haiku, image);
+    return new ContentGeneration("***********", null, List.of(), Optional.empty(), Optional.empty());
   }
 
   public ContentGeneration withImageUrl(String imageUrl) {
-    return new ContentGeneration(userInput, Instant.now().toEpochMilli(), haiku, Optional.of(new Image(imageUrl)));
+    return new ContentGeneration(userInput, generatedAt, progress, haiku, Optional.of(new Image(imageUrl)));
   }
 
   public ContentGeneration withUserInput(String userInput) {
-    return new ContentGeneration(userInput, generatedAt, haiku, image) ;
+    return new ContentGeneration(userInput, generatedAt, progress, haiku, image) ;
   }
 
   public ContentGeneration withHaiku(Haiku haiku) {
-    return new ContentGeneration(userInput, generatedAt, Optional.of(haiku), image);
+    return new ContentGeneration(userInput, generatedAt, progress, Optional.of(haiku), image);
+  }
+
+  public ContentGeneration withGeneratedTime() {
+    return new ContentGeneration(userInput, Instant.now().toEpochMilli(), progress, haiku, image);
+  }
+
+  public ContentGeneration addProgressLine(String line) {
+    var newList = new ArrayList<>(progress);
+    newList.add(line);
+    List<String> immutableList = List.copyOf(newList);
+    return new ContentGeneration(userInput, generatedAt, immutableList, haiku, image);
+  }
+
+  public boolean isComplete() {
+    return image.isPresent();
   }
 }
