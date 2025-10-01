0919cb0caf6338cc00f814b0bfac4e8f0e8d5f33
Renato Cavalcanti <renato@cavalcanti.io>
2025-10-01 18:30:56 +0200

    timed publisher

---
+++ src/main/java/akka/haiku/conference/application/SocialPostConsumer.java
@@ -0,0 +1,58 @@
+package akka.haiku.conference.application;
+
+
+import akka.javasdk.annotations.ComponentId;
+import akka.javasdk.annotations.Consume;
+import akka.javasdk.client.ComponentClient;
+import akka.javasdk.consumer.Consumer;
+import akka.javasdk.timer.TimerScheduler;
+
+import java.time.Duration;
+
+
+/**
+ * Consumes Social post to prepare for scheduling.
+ * They are scheduled as soon as they are created, but will only be posted if they are approved when
+ * the schedule action fires.
+ */
+@ComponentId("social-post-consumer")
+@Consume.FromKeyValueEntity(SocialPostEntity.class)
+public class SocialPostConsumer extends Consumer {
+
+  private final ComponentClient componentClient;
+  private final TimerScheduler  scheduler;
+
+  public SocialPostConsumer(ComponentClient componentClient, TimerScheduler scheduler) {
+    this.componentClient = componentClient;
+    this.scheduler = scheduler;
+  }
+
+  public Effect onChange(SocialPostEntity.SocialPostState postState) {
+
+    var postId = messageContext().eventSubject().get();
+    // as soon as the post is created, we schedule it for publishing
+
+    if (postState.created()) {
+      if (!postState.xHandlers().isEmpty()) {
+        scheduler.createSingleTimer(
+          "x:" + postId,
+          Duration.ofSeconds(10),
+          componentClient.forTimedAction()
+            .method(PostPublisherAction::publishOnX)
+            .deferred(postId)
+        );
+      }
+
+      if (!postState.bskyHandlers().isEmpty()) {
+        scheduler.createSingleTimer(
+          "x:" + postId,
+          Duration.ofSeconds(10),
+          componentClient.forTimedAction()
+            .method(PostPublisherAction::publishOnX)
+            .deferred(postId)
+        );
+      }
+    }
+    return effects().done();
+  }
+}
--- src/main/java/akka/haiku/conference/application/SocialPostEntity.java
+++ src/main/java/akka/haiku/conference/application/SocialPostEntity.java
@@ -20,54 +20,69 @@ public class SocialPostEntity extends KeyValueEntity<SocialPostEntity.SocialPost
                                 List<String> tags,
                                 List<String> xHandlers,
                                 List<String> bskyHandlers,
-                                boolean rejected,
-                                boolean published) {
+                                Status status) {
 
+    public boolean approved() {
+      return status == Status.APPROVED;
+    }
+
+
+    public boolean created() {
+      return status == Status.CREATED;
+    }
+
+    public enum Status {
+      CREATED,
+      REJECTED,
+      APPROVED
+    }
     public static SocialPostState of(String post, String imageUrl, List<String> tags,
                                      List<String> xHandlers,
                                      List<String> bskyHandlers) {
-      return new SocialPostState(post, imageUrl, tags, xHandlers, bskyHandlers, false, false);
+      return new SocialPostState(post, imageUrl, tags, xHandlers, bskyHandlers, Status.CREATED);
       }
 
 
       public SocialPostState asRejected() {
-        return new SocialPostState(post, imageUrl, tags, xHandlers, bskyHandlers, true, published);
+        return new SocialPostState(post, imageUrl, tags, xHandlers, bskyHandlers, Status.REJECTED);
       }
 
-    public SocialPostState asPublished() {
-      return new SocialPostState(post, imageUrl, tags, xHandlers, bskyHandlers, rejected, true);
+    public SocialPostState asApproved() {
+      return new SocialPostState(post, imageUrl, tags, xHandlers, bskyHandlers, Status.APPROVED);
     }
+
   }
 
-    public Effect<Done> createPost(SocialPostState post) {
-        log.info("Creating social post {}", commandContext().entityId());
-        // we allow updates, this allows us to repost it
-        return effects().updateState(post).thenReply(Done.done());
+  public Effect<Done> createPost(SocialPostState post) {
+      log.info("Creating social post {}", commandContext().entityId());
+      // we allow updates, this allows us to repost it
+      return effects().updateState(post).thenReply(Done.done());
+  }
+
+  public Effect<SocialPostState> getPost() {
+    SocialPostState state = currentState();
+    if (state == null) {
+      return effects().error("Post not found: " + commandContext().entityId());
+    } else {
+      return effects().reply(state);
     }
+  }
 
     public Effect<Done> approvePost() {
         SocialPostState state = currentState();
         if (state == null) {
             return effects().error("Post not found: " + commandContext().entityId());
-        } else if (state.published()) {
+        } else if (state.approved()) {
             return effects().reply(Done.done());
         } else {
-            SocialPostState updated = state.asPublished();
+            SocialPostState updated = state.asApproved();
             log.info("Post published {}", commandContext().entityId());
             return effects().updateState(updated).thenReply(Done.done());
         }
     }
 
-    public Effect<SocialPostState> getPost() {
-        SocialPostState state = currentState();
-        if (state == null) {
-            return effects().error("Post not found: " + commandContext().entityId());
-        } else {
-            return effects().reply(state);
-        }
-    }
 
-    public Effect<Done> rejectPost() {
+  public Effect<Done> rejectPost() {
       return effects()
         .updateState(currentState().asRejected())
         .thenReply(Done.done());
